6efe1743e488ac67453115ea1720020e4f8e4cbf
Julien Fontanet <julien.fontanet@isonoe.net>
2015-03-27 13:51:46 +0100

    Various updates.

--- 06-files/files.js
+++ 06-files/files.js
@@ -2,6 +2,41 @@
 
 var fs = require('fs');
 
+// Ce programme créé pour certains exemples des fichiers temporaires
+// qui n'ont pas vocation à rester une fois l'exécution terminée.
+//
+// La fonction `cleanOnExit()` permet justement d'enregistrer ces
+// fichiers afin de les supprimer automatiquement à la fin de
+// l'exécution.
+var cleanOnExit = (function () {
+  var rimraf = require('rimraf').sync;
+  var entries = {};
+
+  process.on('exit', function () {
+    var entry;
+    for (entry in entries) {
+      try {
+        rimraf(entry);
+      } catch (e) {}
+    }
+  });
+
+  return function cleanOnExit(entry) {
+    entries[entry] = true;
+  };
+})();
+
+// Cette fonction retourne un chemin temporaire unique et l'enregistre
+// pour nettoyage avec la fonction `cleanOnExit()`.
+var i = 0;
+function getTmpPath() {
+  var path = __dirname + '/.tmp-' + (i++);
+
+  cleanOnExit(path);
+
+  return path;
+}
+
 //====================================================================
 // Récupération des méta-données.
 
@@ -77,18 +112,77 @@ fs.readFile(__filename, function (error, content) {
 //====================================================================
 // Écriture d'un fichier.
 
-var tmpFile = __dirname + '/tmp-file';
-
-fs.writeFile(tmpFile, 'mon contenu', function (error) {
+fs.writeFile(getTmpPath(), 'mon contenu', function (error) {
   if (error) {
     console.error('writeFile: échec de l\'écriture', error);
   } else {
     console.log('writeFile: fichier écrit');
   }
-
-  fs.unlink(tmpFile, function () {});
 });
 
 //====================================================================
 // Lecture et écriture via les flux.
 
+// Pour plus d'information sur les flux, voir le chapitre 5.
+
+// `createReadStream()` créé un flux de lecture qui va émettre le
+// `contenu du fichier demandé, ici le fichier courant (`__filename`).
+//
+// Note : le fichier n'est pas réellement lu tant que le flux n'est
+// pas consommé par un flux d'écriture ce qui limite l'engorgement
+// mémoire (principe de pression inverse).
+fs.createReadStream(__filename)
+
+  // La méthode `pipe()` connecte le flux de lecture à un flux
+  // d'écriture.
+  .pipe(
+
+    // `createWriteStream()` créé un flux d'écriture qui va
+    // `enregistrer toutes les données qui lui sont fournies dans le
+    // `fichier demandé, ici un fichier temporaire.
+    fs.createWriteStream(getTmpPath())
+  )
+;
+
+//====================================================================
+// Troncature d'un fichier.
+
+// `truncate()` augmente ou diminue la taille d'un fichier pour qu'il
+// `corresponde à la taille demandée.
+//
+// Si la taille demandée est inférieure à la taille actuelle alors le
+// contenu du fichier est tronqué à la taille demandée.
+//
+// Si la taille demandée est supérieure à la taille actuelle alors le
+// contenu est étendu avec des octets nuls (\0).
+//
+// Note : le fichier doit exister.
+fs.truncate(getTmpPath(), 1e3, function (error) {
+  if (error) {
+    console.error('truncate: échec de la troncature', error);
+  } else {
+    console.log('truncate: fichier tronqué');
+  }
+});
+
+//====================================================================
+// Renommage d'un fichier.
+
+fs.rename('foo.txt', 'bar.txt', function (error) {
+  if (error) {
+    console.error('échec du renommage du fichier', error);
+  } else {
+    console.log('fichier renommé');
+  }
+});
+
+//====================================================================
+// Suppression d'un fichier.
+
+fs.unlink(getTmpPath(), function (error) {
+  if (error) {
+    console.error('échec de la suppression du fichier', error);
+  } else {
+    console.log('fichier supprimé');
+  }
+});
